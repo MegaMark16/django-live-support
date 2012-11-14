9200d7514d1de748a1e5b1245ab38618eeb7b941
Mark Ransom <mark@DjangoLiveSupport.(none)>
2012-11-14 22:19:01 +0000

    Updated admin.py to be pep8 complient

--- live_support/admin.py
+++ live_support/admin.py
@@ -3,11 +3,12 @@ from django.conf import settings
 
 from live_support.models import Chat, ChatMessage
 
+
 class ChatAdmin(admin.ModelAdmin):
-    list_display = ('id','name','started',)
-    list_display_links = ('id','name',)
+    list_display = ('id', 'name', 'started',)
+    list_display_links = ('id', 'name',)
     list_filter = ('started',)
-    
+
     class Media:
         js = (
             "live_support/js/live_support_admin.js",
@@ -16,19 +17,18 @@ class ChatAdmin(admin.ModelAdmin):
         css = {
             'all': ('live_support/css/live_support.css',),
         }
-    
+
     def changelist_view(self, request, extra_context=None):
-        pending_chats = Chat.objects.filter(ended=None, agents=None).order_by('-started')
-        active_chats = Chat.objects.filter(ended=None).filter(agents=request.user)
+        pending_chats = Chat.objects.filter(ended=None, agents=None)\
+            .order_by('-started')
+        active_chats = Chat.objects.filter(ended=None)\
+            .filter(agents=request.user)
         c = {
             'pending_chats': pending_chats,
             'active_chats': active_chats,
         }
-        print "ACTIVE CHATS: %s" % active_chats
         return super(ChatAdmin, self).changelist_view(request, extra_context=c)
 
 
 admin.site.register(Chat, ChatAdmin)
 admin.site.register(ChatMessage)
-
-
